eebbdbce5a22e7c7f3771688c5997c85d707d172
Antony Root <antoneh@gmail.com>
2015-11-19 15:30:22 -0500

    implemented generic value swap

--- main.c
+++ main.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
 void intswap(int *x, int *y);
-void swap(void *a, void *b);
+void swap(void *a, void *b, size_t size);
 
 int main(int argc, char *argv[])
 {
 	int x = 34567738;
 	int y = -90098733;
+
 	printf("x = %.8X , y = %.8X\n", x, y);
 	printf("swapping ints ...\n");
 	intswap(&x, &y);
 	printf("x = %.8X , y = %.8X\n", x, y);
+	printf("swapping void* values ...\n");
+	swap(&x, &y, sizeof(int));
+	printf("x = %.8X , y = %.8X\n", x, y);
+
 
 }
 
-void swap(void *a, void *b)
+void swap(void *pa, void *pb, size_t size)
 {
-	//write generic
+	char buffer[size];
+	memcpy(buffer, pa, size);
+	memcpy(pa, pb, size);
+	memcpy(pb, buffer, size);
 }
 void intswap(int *px, int *py)/*{{{*/
 {
