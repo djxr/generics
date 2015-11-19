8e528313b1cd2bdabf186070ec54f515b7820a98
Antony Root <antoneh@gmail.com>
2015-11-19 14:21:06 -0500

    cleaned up names

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-void swap(int *x, int *y);
-void g_swap(void *a, void *b);
+void intswap(int *x, int *y);
+void swap(void *a, void *b);
 
 int main(int argc, char *argv[])
 {
 	int x = 34567738;
 	int y = -90098733;
 	printf("x = %.8X , y = %.8X\n", x, y);
-	printf("swapping...\n");
-	swap(&x, &y);
+	printf("swapping ints ...\n");
+	intswap(&x, &y);
 	printf("x = %.8X , y = %.8X\n", x, y);
 
 }
 
-void g_swap(void *a, void *b)
+void swap(void *a, void *b)
 {
 	//write generic
 }
-void swap(int *px, int *py)/*{{{*/
+void intswap(int *px, int *py)/*{{{*/
 {
 	int temp = *px;
 	*px = *py;
